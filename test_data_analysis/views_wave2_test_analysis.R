e9b25ee40e8b15f4f7e9cfa8c115a15bcabfab35
Hibiki_Sakai <hibiki.sakai7@gmail.com>
2024-05-16 16:24:25 -0500

    Added a check for nonresponse in the VIeWS test data

--- test_data_analysis/views_wave2_test_analysis.R
+++ test_data_analysis/views_wave2_test_analysis.R
@@ -8,3 +8,22 @@ views_test_data <-
 views_test_data <- read.csv(text = capture.output(write.csv(views_test_data, row.names = FALSE)))
 
 table(views_test_data$Age2, useNA = "always")
+
+nonresponse <- views_test_data %>%
+  # summarise across all variables, create a count of NAs
+  summarise(
+    across(
+      everything(),
+      ~ sum(is.na(.)),
+      .names = "count_{col}"
+      )
+    ) %>%
+  # reshape long so that column names are in a questions column and the count of NAs are in a values column
+  pivot_longer(
+    everything(),
+    names_to = "question",
+    values_to = "n_nonresponse"
+  ) %>%
+  # calculate the % nonresponse and sort descending on nonresponse (highest nonresponse on top)
+  mutate(nonresponse_percent = n_nonresponse/nrow(views_test_data) * 100) %>%
+  arrange(desc(nonresponse_percent))
